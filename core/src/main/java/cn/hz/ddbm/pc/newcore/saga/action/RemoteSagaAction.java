8863dbae03c52cac771ae96dc4382ba0a99c7165
mmbbdddd <81521544@qq.com>
2024-09-09 18:08:02 +0800

    rename newcore to core

--- core/src/main/java/cn/hz/ddbm/pc/newcore/saga/action/RemoteSagaAction.java
+++ core/src/main/java/cn/hz/ddbm/pc/newcore/saga/action/RemoteSagaAction.java
@@ -6,6 +6,10 @@ import cn.hz.ddbm.pc.newcore.saga.SagaFlow;
 import cn.hz.ddbm.pc.newcore.saga.SagaState;
 import cn.hz.ddbm.pc.newcore.saga.SagaWorker;
 
+/**
+ * 远程saga方法调用
+ * @param <S>
+ */
 public interface RemoteSagaAction<S extends Enum<S>> extends SagaAction {
     void remoteSaga(FlowContext<SagaFlow<S>, SagaState<S>, SagaWorker<S>> ctx) throws Exception;
 
